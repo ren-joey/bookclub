4e875c56b121c24e6f4e3e813b87cfea44d61277
ChenLin Chu <chenlinchu637@gmail.com>
2022-07-22 17:44:14 +0800

    [更新] Billy - 更新 week2-220719 題目

--- README.md
+++
@@ -1,5 +0,0 @@
-## week1 220712
-https://leetcode.com/problems/recyclable-and-low-fat-products/<br>
-https://leetcode.com/problems/add-two-integers/<br>
-https://leetcode.com/problems/find-total-time-spent-by-each-employee/<br>
-https://leetcode.com/problems/daily-leads-and-partners/<br>
---
+++ problems.js
@@ -0,0 +1,13 @@
+
+const problemList = {
+    // week1-220712
+    1757: 'https://leetcode.com/problems/recyclable-and-low-fat-products/',
+    1741: 'https://leetcode.com/problems/find-total-time-spent-by-each-employee/',
+    2235: 'https://leetcode.com/problems/add-two-integers/',
+    1693: 'https://leetcode.com/problems/daily-leads-and-partners/',
+    // week2-220719
+    1920: 'https://leetcode.com/problems/build-array-from-permutation/',
+    1929: 'https://leetcode.com/problems/concatenation-of-array/',
+    1480: 'https://leetcode.com/problems/running-sum-of-1d-array/',
+    1470: 'https://leetcode.com/problems/shuffle-the-array/'
+};
