7c44377c66d84632456b7879288b01c0a0e5488c
ren-joey <tsuyiren@gmail.com>
2022-07-28 18:02:48 +0800

    [UPT]: week3 questions update

--- problems.js
+++ problems.js
@@ -10,5 +10,11 @@ const problemList = {
     1920: 'https://leetcode.com/problems/build-array-from-permutation/',
     1929: 'https://leetcode.com/problems/concatenation-of-array/',
     1480: 'https://leetcode.com/problems/running-sum-of-1d-array/',
-    1470: 'https://leetcode.com/problems/shuffle-the-array/'
+    1470: 'https://leetcode.com/problems/shuffle-the-array/',
+
+    // week3-220728
+    2011: 'https://leetcode.com/problems/final-value-of-variable-after-performing-operations/',
+    2114: 'https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/',
+    2160: 'https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/',
+    1512: 'https://leetcode.com/problems/number-of-good-pairs/'
 };
